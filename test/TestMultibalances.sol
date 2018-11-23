df8191dbf315c7917ba50b41d313b095d2e1de3c
txhsl <799498265@qq.com>
2018-11-23 18:05:30 +0800

    testMultibalancesUsingDeployedContract removed, for dynamic array is not callable in solidity

--- test/TestMultibalances.sol
+++ test/TestMultibalances.sol
@@ -8,13 +8,5 @@ import "../contracts/MultiBalances.sol";
 contract TestMultibalances {
 
     function testMultibalancesUsingDeployedContract() public {
-        //MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin());
-        //MultiBalances multi = MultiBalances(DeployedAddresses.MetaCoin());
-
-        //uint expected = 100000;
-        //address[] addresses = new address[]();
-        //addresses[0] = tx.origin; 
-
-        //Assert.equal(multi.getBalances(meta, addresses), expected * (10 ** uint256(8)), "Owners should have total 100000 MetaCoin");
     }
 }
