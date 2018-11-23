106c5ba6562e5bde5fcbe29150b1b745560344de
txhsl <799498265@qq.com>
2018-11-23 17:45:53 +0800

    update

--- contracts/MultiBalances.sol
+++ contracts/MultiBalances.sol
@@ -5,7 +5,7 @@ import "./MetaCoin.sol";
 
 contract MultiBalances { 
     function getBalances(address contractAddress, address[] addresses) public view returns (uint256){
-        MetaCoin erc20 = MetaCoin(contractAddress);
+        Token erc20 = Token(contractAddress);
         uint sum = 0;
         uint addrLength = addresses.length;
         uint256[]  memory balances = new uint256[](addrLength);
